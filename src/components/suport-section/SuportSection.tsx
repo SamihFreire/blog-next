f7c504dbe4eac4cc7e5e2ea047fcadde6800272f
samih freire <samihcisne@gmail.com>
2025-11-13 10:02:02 -0300

    feat: criado support section

---
+++ src/components/suport-section/SuportSection.tsx
@@ -0,0 +1,53 @@
+import { HeartHandshake, PaintbrushVertical, Store } from "lucide-react";
+import { PT_Sans_Caption } from "next/font/google"; // Importando a font
+
+// Utilizando a font PT_Sans_Captian do next font google
+const ptSansCaptian = PT_Sans_Caption({
+    subsets: ['latin'],
+    weight: '700'
+})
+
+export function SuportSecion() {
+    return (
+        <section className="pb-8 md:py-10 bg-gray-700">
+            <div className="container flex flex-col items-center gap-12">
+                {/* text-balance: serve para deixar o texto com quebras de linha mais harmoniosas e equilibradas */}
+                <h2 className={`${ptSansCaptian.className} text-balance text-center text-heading-xl text-gray-100`}>Sua loja de afiliados, simples, do jeito que deveria ser</h2>
+
+                <div className="grid gap-6 md:grid-cols-3">
+                    {/* Primeiro Card */}
+                    <div className="flex flex-col text-left gap-2 rounded-lg p-6 md:p-12 bg-blue-400">
+                        <div className="flex h-12 w-12 items-center justify-center rounded-lg bg-blue-300 mb-4">
+                            <PaintbrushVertical className="h-6 w-6 text-white" />
+                        </div>
+
+                        <strong className="text-heading-sm text-gray-100">Personalize seu site</strong>
+                        <p className="text-body-sm text-gray-200">Adicione sua logo, favicon, cores no seu catálago e tenha tudo com a sua cara.</p>
+                    </div>
+
+
+                    {/* Segundo Card */}
+                    <div className="flex flex-col text-left gap-2 rounded-lg p-6 md:p-12 bg-cyan-300">
+                        <div className="flex h-12 w-12 items-center justify-center rounded-lg bg-cyan-200 mb-4">
+                            <Store className="h-6 w-6 text-white" />
+                        </div>
+
+                        <strong className="text-heading-sm text-gray-100">Venda de qualquer loja</strong>
+                        <p className="text-body-sm text-gray-200">Não importa a loja, o Site.Set permite que você insira qualquer link de afiliado.</p>
+                    </div>
+
+
+                    {/* Terceiro Card */}
+                    <div className="flex flex-col text-left gap-2 rounded-lg p-6 md:p-12 bg-blue-400">
+                        <div className="flex h-12 w-12 items-center justify-center rounded-lg bg-blue-300 mb-4">
+                            <HeartHandshake className="h-6 w-6 text-white" />
+                        </div>
+
+                        <strong className="text-heading-sm text-gray-100">Receba suporte amigável</strong>
+                        <p className="text-body-sm text-gray-200">Nossa equipe estará sempre pronta para te atender para ajudar no que for preciso.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { FeatureSection } from "@/components/feature-section";
 import { HeroSection } from "@/components/hero-section";
+import { SuportSecion } from "@/components/suport-section";
 
 export default function HomePage() {
   return (
@@ -7,6 +8,7 @@ export default function HomePage() {
       <article className="flex flex-col">
         <HeroSection />
         <FeatureSection />
+        <SuportSecion />
       </article>
     </>
   );
