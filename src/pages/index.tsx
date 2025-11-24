5a6d28e639ca05a6b6bb854523d1a8b3216d4bc3
samih freire <samihcisne@gmail.com>
2025-11-24 16:24:26 -0300

    chore: configurado contentlayer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { LandingPage } from "../templates/landing-page";
 
+import { allPosts, Post } from "contentlayer/generated";
+
 export default function HomePage() {
   return (
     <LandingPage />
